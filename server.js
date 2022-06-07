45cf7ffd0756eb5b0288899adb62a46468cdc612
Marquis <marquispierre27@gmail.com>
2022-06-07 00:03:16 -0400

    Pushing BMI calculator app to git hub

---
+++ server.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", (req,res)=> {
+    res.sendFile(__dirname + "/index.html")
+    
+})
+
+app.post("/", (req,res)=>{
+    var weight = parseFloat(req.body.Weight)
+    var height = parseFloat(req.body.Height)
+    
+    var bmi = (weight / (height * height) * 703)
+    
+    console.log(weight)
+
+    console.log(bmi)
+
+   res.send("Your BMI is " + bmi) 
+})
+
+
+app.listen(port, () => {
+    console.log("Sever is working bud!!")
+})
